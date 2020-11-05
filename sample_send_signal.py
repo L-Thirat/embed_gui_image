be144fd477343bd8ef4d1ebed606cc475f170907
L-Thirat <terus_limsurut@hotmail.com>
2020-11-05 11:53:08 +0900

    input pin

--- sample_send_signal.py
+++ sample_send_signal.py
@@ -4,13 +4,25 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.ASUS)
 LED = 164
 GPIO.setup(LED,GPIO.OUT)
+
+BTN_input = 167
+# [167, 160, 162]
+
+def control(pin, signal):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.ASUS)
+    GPIO.setup(pin, GPIO.OUT)
+    if signal:
+        GPIO.output(pin, GPIO.HIGH)
+    else:
+        GPIO.output(pin, GPIO.LOW)
+    time.sleep(1)
+
 try:
     while True:
-        print ("led on")
-        GPIO.output(LED,GPIO.HIGH)
-        time.sleep(1)
-        print ("led off")
-        GPIO.output(LED,GPIO.LOW)
-        time.sleep(1)
+        if GPIO.input(BTN_input):
+            control(pin=LED, signal=True)
+        else:
+            control(pin=LED, signal=False)
 except KeyboardInterrupt:
     GPIO.cleanup()
